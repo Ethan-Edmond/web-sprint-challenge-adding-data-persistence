80e54ca26e27e0b924a32d3db404440ccd5a8122
Ethan Edmond <eedmond@uccs.edu>
2021-07-16 10:10:04 -0600

    Added migration for task table

---
+++ data/migrations/20210716160310_add_task_table.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('tasks', tbl => {
+    tbl.increments('task_id');
+    tbl.string('task_description')
+      .notNullable();
+    tbl.string('task_notes');
+    tbl.boolean('task_completed')
+      .defaultTo(false);
+    tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id')
+      .inTable('projects')
+      .onDelete('RESTRICT')
+      .onUpdate('RESTRICT');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('tasks');
+};
