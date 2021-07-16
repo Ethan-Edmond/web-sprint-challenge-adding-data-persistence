56c26f537fa3d64b9e5aaf9fb3c0932a8071347b
Ethan Edmond <eedmond@uccs.edu>
2021-07-16 10:30:33 -0600

    Finished project endpoints, needs validation middleware

--- api/project/router.js
+++ api/project/router.js
@@ -2,13 +2,20 @@
 const router = require('express').Router();
 const Projects = require('./model');
 
-console.log('Projects:', Projects);
 router.get('/', (req, res, next) => {
-  res.json({message: 'wired'});
+  Projects.getAll()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(next);
 });
 
 router.post('/', (req, res, next) => {
-  next();
+  Projects.add(req.body)
+    .then(newProject => {
+      res.json(newProject);
+    })
+    .catch(next);
 });
 
 module.exports = router;
